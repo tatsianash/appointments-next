83929710a8f7df77671e905da9111ca72f55e236
Tatsiana Sh <tatsiana15az@gmail.com>
2023-10-04 12:39:05 -0700

    Add plugins and configurations

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/about",
+        destination: "/",
+      },
+    ];
+  },
+};
